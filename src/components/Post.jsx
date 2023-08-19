b1ee4fda2fc9e1ed13e9f865c064560ea39c8e0d
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-19 20:52:03 -0300

    feat: :sparkles: add new Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,25 @@
+import styles from './Post.module.css';
+
+export function Post(){
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/samsks.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Samuel Vieira</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title='19 de agosto às 20:45h'  dateTime='2023-08-19 20:45:59'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+    )
+}
