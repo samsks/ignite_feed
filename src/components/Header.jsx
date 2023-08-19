078900a1ca461d0898168271ccdb249250915e44
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-19 15:15:09 -0300

    feat: :sparkles: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+import ignitelogo from '../assets/ignite_logo.svg';
+
+export function Header(){
+    return (
+        <header className={styles.header}>
+            <img src={ignitelogo} alt="Logotipo do Ignite" />
+            <strong>Ignite Feed</strong>
+        </header>
+    );
+}
