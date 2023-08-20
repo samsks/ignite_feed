00d56cf621e8c140e2f975ea7475da3abcebc9df
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-20 01:33:58 -0300

    feat: :recycle: refactoring the Sidebar component by adding the Avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import {PencilLine} from 'phosphor-react';
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -9,7 +10,9 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1580777361964-27e9cdd2f838?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" 
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://media.licdn.com/dms/image/D4D03AQEvteIXwNqYtA/profile-displayphoto-shrink_400_400/0/1673893454596?e=1698278400&v=beta&t=WBXTxzwjZDbj1UY4WV41yEV7bRCkvbCt7pJPcYLK4xg" />
+                <Avatar 
+                    src="https://media.licdn.com/dms/image/D4D03AQEvteIXwNqYtA/profile-displayphoto-shrink_400_400/0/1673893454596?e=1698278400&v=beta&t=WBXTxzwjZDbj1UY4WV41yEV7bRCkvbCt7pJPcYLK4xg" 
+                />
                 <strong>Samuel Vieira</strong>
                 <span>Fullstack Developer</span>
             </div>
