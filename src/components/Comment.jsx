effa6a41cb7c5a4edcb3c8c2fa17495c2bb45b94
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-19 23:54:53 -0300

    feat: :lipstick: update font size for trans icon in Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -19,7 +19,7 @@ export function Comment(){
                         </div>
 
                         <button title='Deletar comentário'>
-                            <Trash size={20} />
+                            <Trash size={24} />
                         </button>
                     </header>
 
@@ -28,7 +28,7 @@ export function Comment(){
 
                 <footer>
                     <button>
-                        <ThumbsUp size={20} />{" "}
+                        <ThumbsUp size={20} />
                         Aplaudir <span>20</span>
                     </button>
                 </footer>
