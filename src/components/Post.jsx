60137bd47a4034c3d4590ffa332fc5885c168574
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-20 21:16:38 -0300

    feat: :bug: insert temp keys for render

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -60,8 +60,8 @@ export function Post({author, content, publishedAt}){
 
             <div className={styles.content}>
                 {content.map(line => {
-                    if (line.type === 'paragraph') return <p>{line.content}</p>
-                    else if (line.type === 'link') return <p><a href="#">{line.content}</a></p>
+                    if (line.type === 'paragraph') return <p key={line.content}>{line.content}</p>
+                    else if (line.type === 'link') return <p key={line.content}><a href="#">{line.content}</a></p>
                 })}
             </div>
 
@@ -80,7 +80,7 @@ export function Post({author, content, publishedAt}){
             </form>
 
             <div className={styles.commentList}>
-            {comments.map(comment => <Comment content={comment}/>)}
+            {comments.map(comment => <Comment key={comment} content={comment}/>)}
             </div>
         </article>
     )
