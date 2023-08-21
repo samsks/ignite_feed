3749130d045ae2321c6cf942b71547d8fdeb366f
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-20 23:32:39 -0300

    feat: :white_check_mark: Comment componente receiving onDelete props and inserting in the delete button

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({content}){
+export function Comment({content, onDeleteComment}){
+
+    function handleDeleteComment(){
+        onDeleteComment(content)
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar 
@@ -22,8 +27,11 @@ export function Comment({content}){
                             </time>
                         </div>
 
-                        <button title='Deletar comentário'>
-                            <Trash size={24} />
+                        <button 
+                            onClick={handleDeleteComment}
+                            title='Deletar comentário'
+                        >
+                                <Trash size={24} />
                         </button>
                     </header>
 
